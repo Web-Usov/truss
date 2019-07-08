4900a36b6555bb83972c77c09f76a35f6f502dbc
Web-Usov <web.usov@ya.ru>
2019-07-08 15:33:28 +0400

    add env params

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,25 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { history } from './utils';
 import { mainReducer } from './reducers';
-import { Router } from 'react-router-dom';
+import { HashRouter, Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const isGitHubPages = process.env.REACT_APP_GHP || false;
 const store = createStore(mainReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <App />
-        </Router>
+        {
+            isGitHubPages ?
+                <HashRouter>
+                    <App />
+                </HashRouter> :
+                <Router history={history}>
+                    <App />
+                </Router>
+        }
+
     </Provider>,
     document.getElementById('root')
 );
