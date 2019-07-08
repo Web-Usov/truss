e64e68c8fcac22c9632157cb79c231afd8cc6ab8
Web-Usov <web.usov@ya.ru>
2019-07-08 04:11:42 +0400

    Updates

---
+++ precache-manifest.01ad27bbb7a1a6db52f7b3e1570a84e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352cf05383af732d6a44ece0fb34f36a",
+    "url": "/truss/index.html"
+  },
+  {
+    "revision": "309664b66d8ae0b5a5ad",
+    "url": "/truss/static/css/main.c391d196.chunk.css"
+  },
+  {
+    "revision": "2950ce92600af79f31ee",
+    "url": "/truss/static/js/2.b6bba7a9.chunk.js"
+  },
+  {
+    "revision": "309664b66d8ae0b5a5ad",
+    "url": "/truss/static/js/main.8548fa52.chunk.js"
+  },
+  {
+    "revision": "bfe8d3fde6021925c1a2",
+    "url": "/truss/static/js/runtime~main.6c9d2935.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/truss/static/media/logo.5d5d9eef.svg"
+  }
+]);
