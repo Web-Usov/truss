f1f89e6757cb5c69005309d1c4d64e7f88b16a6d
Web-Usov <web.usov@ya.ru>
2019-07-30 04:02:26 +0400

    Updates

---
+++ precache-manifest.03749e5aa6cfaafa7bbdb8a3faecce8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7ce2dbb7e145860e30bbf8ad8c8384",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff49865647429ec9aef1",
+    "url": "./static/css/main.84bbde08.chunk.css"
+  },
+  {
+    "revision": "bb5396a7f3f88ab46087",
+    "url": "./static/js/2.b9157cd1.chunk.js"
+  },
+  {
+    "revision": "ff49865647429ec9aef1",
+    "url": "./static/js/main.695e5032.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7d581a654d5a69c70c174a3132c8dbe6",
+    "url": "./static/media/airBridge.7d581a65.png"
+  },
+  {
+    "revision": "e53bced329191090fcaed0d8f6720beb",
+    "url": "./static/media/bridge.e53bced3.png"
+  },
+  {
+    "revision": "68e274b2e3be8a0f5f0206a036e174af",
+    "url": "./static/media/icon.68e274b2.png"
+  },
+  {
+    "revision": "0619e300ac6f50c3d3dfcb9ddad6d135",
+    "url": "./static/media/ogImage.0619e300.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://web-usov.github.io/truss/precache-manifest.2652a5b16fe3f87f8d8f76b0feb9d1a5.js"
+  "./precache-manifest.03749e5aa6cfaafa7bbdb8a3faecce8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://web-usov.github.io/truss/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
