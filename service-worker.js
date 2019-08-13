37ff31dcba2eab1459a5d0e93906c060588467f5
Alexey Usov <usov@mercury.office>
2019-08-13 17:41:35 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://web-usov.github.io/truss/precache-manifest.90daf01abd9749e75056f5e07ef2d76d.js"
+  "https://web-usov.github.io/truss/precache-manifest.e02f69324db9cc63a05a5eb9706850a0.js"
 );
 
 self.addEventListener('message', (event) => {
